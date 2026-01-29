175a443d78073e526b144c1fdb99bf1d7b01c884
grassfinn <theharper@live.com>
2026-01-29 12:45:53 -0500

    Fix Business Directory

--- directory/cardTemplate.js
+++ directory/cardTemplate.js
@@ -16,30 +16,26 @@ export default class cardTemplate extends HTMLElement {
     title.className = 'name';
     title.textContent = obj.name;
     const address = createElement('p');
-    address.className = 'address';    
+    address.className = 'address';
     address.textContent = obj.address;
     const website = createElement('a');
     const websiteLink = createElement('a');
     website.href = obj.website;
-    websiteLink.textContent = `Click here to go to ${obj.name} website`
-    websiteLink.href = obj.website
+    websiteLink.textContent = `Click here to go to ${obj.name} website`;
+    websiteLink.href = obj.website;
     title.append(website);
     parent.append(title, websiteLink, address, website);
     if (obj.image) {
-      console.log("image");
-      
       image = createElement('img');
       image.alt = obj.name;
       image.src = obj.image;
       website.append(image);
       parent.append(website);
-    }else {
-      
+    } else {
     }
     return parent;
   }
   #generateCards(arr) {
-
     return arr.map((data) => this.#createCard(data));
   }
 
@@ -59,24 +55,26 @@ export default class cardTemplate extends HTMLElement {
     const currentData = this.businesses[`${window.location.pathname}`];
 
     if (urlLocation === '/directory/business.html') {
-      const cardTitle = document.querySelector('#card-title')
+      const cardTitle = document.querySelector('#card-title');
       let currentCategory = '';
       categoryContainer.classList.add('business-categories');
       const categories = Object.entries(currentData.categories);
       const titles = categories.map((category) => {
-        const cardParent = document.createElement('div')
+        const cardParent = document.createElement('div');
         const div = document.createElement('div');
         const cardImg = document.createElement('img');
         cardImg.src = category[1].img;
         div.className = 'card';
         const title = document.createElement('h3');
-        title.className = 'business-category'
+        title.className = 'business-category';
         title.textContent = category[0];
         div.style.backgroundImage = `url('${category[1].img}')`;
-        
+
         div.addEventListener('click', (e) => {
-          currentCategory = e.target.textContent;
-          cardTitle.textContent = e.target.textContent
+          currentCategory = e.target.parentElement.children[0].textContent;
+
+          cardTitle.textContent = currentCategory;
+
           const currentData =
             data[window.location.pathname].categories[currentCategory]
               .businesses;
@@ -84,12 +82,12 @@ export default class cardTemplate extends HTMLElement {
           categoryContainer.style.display = 'none';
           const clickedCategory = currentData.map((business) => {
             const newCard = this.#createCard(business);
-            
+
             return newCard;
           });
           businessContainer.append(...clickedCategory);
         });
-        cardParent.append(title,div);
+        cardParent.append(title, div);
         return cardParent;
       });
       categoryContainer.append(...titles);
